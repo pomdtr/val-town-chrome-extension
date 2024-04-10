2764f0e70b661ae9d3d891ed2ee94d69dbaa03e0
pomdtr <achille.lacoin@gmail.com>
2024-04-10 15:40:11 +0200

    fix default config syntax error

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export async function loadConfig(): Promise<Config> {
   const storage = await chrome.storage.local.get(["config"]);
-  return JSON.parse(storage.config || `{ rootVal: "pomdtr/tree_example" }`);
+  return JSON.parse(storage.config || `{ "rootVal": "pomdtr/tree_example" }`);
 }
 export type Config = {
   token: string;
